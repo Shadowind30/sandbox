3a5a1342ec3300cd5ea853a8f6796657ebdcfe87
Shadowind30 <shadowind30@gmail.com>
2021-08-19 11:02:11 -0400

    debugging

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   /// Return if the request is made by other than the web page
-  if (!event.request.url.includes('http')) return;
-  event.respondWith(async function() {
-    try{
-      debugger
-      var res = await fetch(event.request);
-      var cache = await caches.open('cache');
-      /// Return if the request is made by other than the web page
-      if (!event.request.url.includes('http')) return;
-       cache.put(event.request.url, res.clone());
-       return res;
-     }
-     catch(error){
-       return caches.match(event.request);
-      }
-    }());
+  if (event.request.url.includes("http")) {
+    event.respondWith(
+      (async function () {
+        try {
+          debugger;
+          var res = await fetch(event.request);
+          var cache = await caches.open("cache");
+          /// Return if the request is made by other than the web page
+          if (!event.request.url.includes("http")) return;
+          cache.put(event.request.url, res.clone());
+          return res;
+        } catch (error) {
+          return caches.match(event.request);
+        }
+      })()
+    );
+  }
 });
