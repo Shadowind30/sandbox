a03c56f2587e04dec22a627f9527b692908d6795
Shadowind30 <shadowind30@gmail.com>
2021-08-19 19:19:23 -0400

    feat: add IOS Icon

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
 self.addEventListener("fetch", function (event) {
-  debugger
   /// Return if the request is made by other than the web page
   if (event.request.url.includes("http")) {
     event.respondWith(
       (async function () {
         try {
-          debugger;
           var res = await fetch(event.request);
           var cache = await caches.open("cache");
           cache.put(event.request.url, res.clone());
